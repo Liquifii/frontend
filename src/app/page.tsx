8eda6b33a61f1fd6423e2249077608245199412e
AbolareRoheemah <roheemahabo@gmail.com>
2026-01-09 09:32:18 +0100

    installed farcaster SDK and wagmi connector. Called ready in root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,30 @@ import Footer from '@/components/footer'
 import HeroSection from '@/components/hero'
 import Navbar from '@/components/navbar'
 import Steps from '@/components/steps'
+import { useEffect, useState } from 'react'
+import { sdk } from '@farcaster/miniapp-sdk'
 
 function App() {
-
+  const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+  
+  useEffect(() => {
+    const load = async () => {
+      sdk.actions.ready();
+    };
+    if (sdk && !isSDKLoaded) {
+      setIsSDKLoaded(true);
+      load();
+    }
+  }, [isSDKLoaded]);
 
   return (
     <>
- <Navbar />
- <HeroSection />
- <Features />
- <Steps />
-<CTASection />
- <Footer/>
+      <Navbar />
+      <HeroSection />
+      <Features />
+      <Steps />
+      <CTASection />
+      <Footer />
     </>
   )
 }
