4d14e57eccda8c1d6a4b0df44ea7ef63bfe8fdda
AbolareRoheemah <roheemahabo@gmail.com>
2026-01-11 12:47:37 +0100

    updated account association

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -3,12 +3,10 @@ export async function GET() {
   
     const config = {
       accountAssociation: {
-        header:
-          "",
-        payload: "",
-        signature:
-          "",
-      },
+          "header": "eyJmaWQiOjEwMjQ1MjMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHg5RDY0NmViRjVFRTcyQzhkNzJFNGJlMzBGYTdGMTA2MDA0QjBFOTU0In0",
+          "payload": "eyJkb21haW4iOiJmcm9udGVuZC1laWdodC1waS00OC52ZXJjZWwuYXBwIn0",
+          "signature": "w+stZIQMiOwMfq4IhyGtdff4tAgCw42lOPvqmRUw2n11ILwTxJOS5rDZVa2KFW+SWmcVjiI7T5hfp8JpSbJAjhs="
+        },
       frame: {
         version: "1",
         name: "Attestify",
